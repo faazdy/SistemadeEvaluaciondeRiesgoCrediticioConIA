57ce30b1b66bf2b91ff4612d290e0bf351fd4890
880515_HaroldPinto <harolpintosuarez54321@gmail.com>
2025-04-09 20:49:17 -0500

    creacion de proyecto spring con dependecias de jpa, web, security, dev tools, mysql, esta creado con maven jdk21

---
+++ riesgocrediticio/src/main/java/com/rc_app/riesgocrediticio/RiesgocrediticioApplication.java
@@ -0,0 +1,13 @@
+package com.rc_app.riesgocrediticio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiesgocrediticioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiesgocrediticioApplication.class, args);
+	}
+
+}
---
+++ riesgocrediticio/src/test/java/com/rc_app/riesgocrediticio/RiesgocrediticioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc_app.riesgocrediticio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiesgocrediticioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
