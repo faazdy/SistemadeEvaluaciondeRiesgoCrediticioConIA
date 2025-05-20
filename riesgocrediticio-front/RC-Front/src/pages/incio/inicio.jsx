7bd48f10950f4ee84c4ac63b4af3f84f4d3cc37b
Lauravega12 <vegalauraa022@gmail.com>
2025-05-20 00:49:58 -0500

    -m 'Se componetizó la card para el resumen de estado financiero'

--- riesgocrediticio-front/RC-Front/src/pages/incio/inicio.jsx
+++ riesgocrediticio-front/RC-Front/src/pages/incio/inicio.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navbar/navbar";
 import Riesgo from "../../components/tipoRiesgo/riesgo";
 import Chatbot from "../../components/chatbot/chatbot";
 import Footer from "../../components/footer/footer";
+import Review from "../../components/cardPerfil/Review";
 
 function Inicio() {
   return (
@@ -21,29 +22,8 @@ function Inicio() {
             <br /> <b>¡Ella se encarga del análisis por ti!</b>
           </p>
         </article>
-        <div class=" inicio-Perfil">
-          <header class=" inicio-Perfil-header">
-            <p>Actualizacion 02/02/2025 </p>
-            <span class="title">Riesgo Regular</span>
-            <br /> <br /> Tiene algunos pagos tardíos ocasionales.Su
-            endeudamiento es moderado respecto a ingresos. Probabilidad
-            razonable de cumplir obligaciones.
-          </header>
-          <div class=" inicio-Perfil-author">
-            <a class="author-avatar" href="#">
-              <span></span>
-            </a>
-            <svg class="half-circle" viewBox="0 0 106 57">
-              <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
-            </svg>
-            <div class="author-name">
-              <div class="author-name-prefix">Harold</div> estibenson
-            </div>
-          </div>
-          <div class="tags">
-            <a href="#">21 años</a>
-          </div>
-        </div>
+
+        <Review />
       </div>
       <h3 class="inicio-TpsRiesgo">Tipos De Riesgo</h3>
       <div className="inicio-CrRsgos">
